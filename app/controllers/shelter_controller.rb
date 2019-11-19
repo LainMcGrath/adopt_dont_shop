537cd26d0f4b0ee8f6e0fea9f4cf68a043f3439a
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-18 19:57:00 -0700

    Add shetler index and create

---
+++ app/controllers/shelter_controller.rb
@@ -0,0 +1,20 @@
+class ShelterController < ApplicationController
+  def index
+    @shelters = Shelter.all
+  end
+
+  def new
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip],
+      })
+      shelter.save
+      redirect_to '/tasks'
+  end
+end
