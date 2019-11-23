6cec408e5f59ba0df3674f5bef4cee921c92ba85
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-23 11:54:19 -0700

    Add functioning create new pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,4 +12,20 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @shelter = Shelter.find(params[:shelter_id])
+  end
+
+  def create
+    shelter = Shelter.find(params[:shelter_id])
+    shelter.pets.create!(pet_params)
+
+    redirect_to "/shelters/#{@shelter.id}/pets"
+  end
+
+  private
+    def pet_params
+      params.permit(:name, :description, :approximate_age)
+    end
 end
