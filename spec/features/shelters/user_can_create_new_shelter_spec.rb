8073bfc8fa61ff129263cf20bdadb287a44f001e
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-23 08:40:10 -0700

    Add refactored create new shelter test

--- spec/features/shelters/user_can_create_new_shelter_spec.rb
+++ spec/features/shelters/user_can_create_new_shelter_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'create new shelter', type: :feature do
 
   it "can create a new shelter" do
     visit "/shelters/new"
-    fill_in 'shelter[name]', with: "New shelter!"
-    fill_in 'shelter[address]', with: "123 Main St"
-    fill_in 'shelter[city]', with: "Denver"
-    fill_in 'shelter[state]', with: "CO"
-    fill_in 'shelter[zip]', with: "80206"
-    # click_button('submit')
-    # expect(page).to have_current_path("/shelters")
+    fill_in :name, with: "New shelter!"
+    fill_in :address, with: "123 Main St"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80206"
+    click_button('Create new shelter')
+    expect(page).to have_current_path("/shelters")
   end
 end
