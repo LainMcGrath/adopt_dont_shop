4772b91f801bfc9d3c54bf92be957bdfa0f47ae2
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-23 19:33:19 -0700

    Add new pet test

--- spec/features/pets/user_can_create_a_new_pet_spec.rb
+++ spec/features/pets/user_can_create_a_new_pet_spec.rb
@@ -11,9 +11,14 @@ RSpec.describe "User can create a new pet", type: :feature do
 
     visit "/shelters/#{shelter_1.id}/pets/new"
 
+    fill_in :image_url, with: "https://www.severnedgevets.co.uk/sites/default/files/styles/medium/public/guides/puppy_0.png?itok=qQYZZd9K"
     fill_in :name, with: "Spuds"
+    fill_in :sex, with: "male"
     fill_in :description, with: "A cute cuddy pup!"
     fill_in :approximate_age, with: "3"
+
+    click_button('Create new pet')
+    expect(page).to have_current_path("/shelters/#{shelter_1.id}/pets")
   end
 
 end
